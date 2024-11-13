cba57e6aaadc82f7f730237cdb8b8f7f63a58556
Facundo Lastra <flastra@SYSPRO01.LOCAL>
2024-11-13 17:06:58 -0300

    Update 17:06

--- stream_impo_historico.py
+++ stream_impo_historico.py
@@ -4,9 +4,22 @@ import pandas as pd
 def fetch_data_impo_historico():
     arribos_impo_historico = pd.read_csv('data/arribos_impo_historico.csv')
     return arribos_impo_historico
-    
+
 def show_page_impo_historico():
     arribos_impo_historico = fetch_data_impo_historico()
+    
     st.title("Histórico - Operaciones de IMPO")
     st.subheader("Arribos de contenedores")
-    st.dataframe(arribos_impo_historico, hide_index=True, use_container_width=True)
+    
+    # Convert 'Fecha' column to datetime
+    arribos_impo_historico['Fecha'] = pd.to_datetime(arribos_impo_historico['Fecha'])
+    
+    # Date range filter
+    start_date = st.date_input("Start date", value=arribos_impo_historico['Fecha'].min())
+    end_date = st.date_input("End date", value=arribos_impo_historico['Fecha'].max())
+    
+    # Filter data based on selected date range
+    filtered_data = arribos_impo_historico[(arribos_impo_historico['Fecha'] >= pd.to_datetime(start_date)) & 
+                                           (arribos_impo_historico['Fecha'] <= pd.to_datetime(end_date))]
+    
+    st.dataframe(filtered_data, hide_index=True, use_container_width=True)
