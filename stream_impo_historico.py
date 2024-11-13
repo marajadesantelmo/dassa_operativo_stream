f4366378425ae81a67be8cee1733bd6eaee16308
Facundo Lastra <flastra@SYSPRO01.LOCAL>
2024-11-13 17:21:01 -0300

    Update 17:21

--- stream_impo_historico.py
+++ stream_impo_historico.py
@@ -15,11 +15,17 @@ def show_page_impo_historico():
     arribos_impo_historico['Fecha'] = pd.to_datetime(arribos_impo_historico['Fecha'])
     
     # Date range filter
-    start_date = st.date_input("Start date", value=arribos_impo_historico['Fecha'].min())
-    end_date = st.date_input("End date", value=arribos_impo_historico['Fecha'].max())
+    col1, col2 = st.columns(2)
+    with col1:
+        start_date = st.date_input("Fecha Inicio", value=arribos_impo_historico['Fecha'].min())
+    with col2:
+        end_date = st.date_input("Fecha Fin", value=arribos_impo_historico['Fecha'].max())
     
     # Filter data based on selected date range
     filtered_data = arribos_impo_historico[(arribos_impo_historico['Fecha'] >= pd.to_datetime(start_date)) & 
                                            (arribos_impo_historico['Fecha'] <= pd.to_datetime(end_date))]
+    
+    # Format 'Fecha' column to show only date part in Spanish format
+    filtered_data['Fecha'] = filtered_data['Fecha'].dt.strftime('%d/%m/%Y')
     
     st.dataframe(filtered_data, hide_index=True, use_container_width=True)
