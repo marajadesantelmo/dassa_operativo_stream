a0872fd7b2d151f957705a6bbbeabd53a08fe510
Facundo Lastra <flastra@SYSPRO01.LOCAL>
2024-11-13 14:21:00 -0300

    Update 14:21

--- alertas_automaticas.py
+++ alertas_automaticas.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import smtplib
 from email.mime.text import MIMEText
+import re
 
 arribados = pd.read_csv('//dc01/Usuarios/PowerBI/flastra/Documents/dassa_operativo_stream/arribos_historico_horarios.csv')
 
@@ -10,14 +11,21 @@ if alertas.empty:
     print("No hay alertas para enviar")
     exit()
 
+clientes = pd.read_csv('//dc01/Usuarios/PowerBI/flastra/Documents/dassa_operativo_stream/contactos_clientes.csv')
 
+clientes['email'] = clientes['email'].str.replace(';', ',')
+def add_space_after_comma(email):
+    return re.sub(r',(?=\S)', ', ', email)
+
+clientes['email'] = clientes['email'].apply(add_space_after_comma)
+clientes['email'] = clientes['email'].str.replace('"', '')
 
 # Function to send email
-def send_email(alertas):
+def send_email(alertas, mails):
     msg = MIMEText(f"El contendedor {alertas['Contenedor']} del cliente {alertas['Cliente']} ")
     msg['Subject'] = 'Contenedor Arribado'
     msg['From'] = "auto@dassa.com.ar"
-    msg['To'] = 'marajadesantelmo@gmail.com'
+    msg['To'] = mails
 
     with smtplib.SMTP("smtp.gmail.com", 587) as server:
         server.starttls() 
@@ -25,6 +33,7 @@ def send_email(alertas):
         server.sendmail(msg['From'], [msg['To']], msg.as_string())
 
 for _, row in alertas.iterrows():
+    mails = clientes[clientes['clie_nro'] == row['Cliente']]['email'].values[0]
     send_email(row)
 
 alertas['alerta_enviada'] = 1
