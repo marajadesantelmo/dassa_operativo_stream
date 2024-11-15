79072850244b6794b95f48aeb1cf3d9f7cb557dd
Facundo Lastra <flastra@SYSPRO01.LOCAL>
2024-11-15 16:31:01 -0300

    Update 16:31

--- stream_expo_historico.py
+++ stream_expo_historico.py
@@ -17,8 +17,8 @@ def show_page_expo_historico():
     arribos_expo_ctns_historico['Fecha'] = pd.to_datetime(arribos_expo_ctns_historico['Fecha'])
     historico_verificaciones_expo['Dia'] = pd.to_datetime(historico_verificaciones_expo['Dia'])
     historico_otros_expo['Dia'] = pd.to_datetime(historico_otros_expo['Dia'])
-    historico_remisiones['Fecha'] = pd.to_datetime(historico_remisiones['Fecha'])
-    historico_consolidados['Fecha'] = pd.to_datetime(historico_consolidados['Fecha'])
+    historico_remisiones['Dia'] = pd.to_datetime(historico_remisiones['Dia'])
+    historico_consolidados['Dia'] = pd.to_datetime(historico_consolidados['Dia'])
     
     col_logo, col_title = st.columns([2, 5])
     with col_logo:
@@ -31,17 +31,21 @@ def show_page_expo_historico():
 
     with col1: 
         st.subheader("Arribos de carga")
-        col1_1, col1_2 = st.columns(2)
+        col1_1, col1_2, col1_3 = st.columns(3)
         with col1_1:
-            start_date_arribos = st.date_input("Fecha Inicio", value=arribos_expo_carga_historico['Fecha'].min(), key='start_date_arribos')
-            st.write(f"Fecha Inicio: {start_date_arribos.strftime('%d/%m/%Y')}")
+            start_date_arribos_carga = st.date_input("Fecha Inicio", value=arribos_expo_carga_historico['Fecha'].min(), key='start_date_arribos_carga')
+            st.write(f"Fecha Inicio: {start_date_arribos_carga.strftime('%d/%m/%Y')}")
         with col1_2:
-            end_date_arribos = st.date_input("Fecha Fin", value=arribos_expo_carga_historico['Fecha'].max(), key='end_date_arribos')
-            st.write(f"Fecha Fin: {end_date_arribos.strftime('%d/%m/%Y')}")
+            end_date_arribos_cargas = st.date_input("Fecha Fin", value=arribos_expo_carga_historico['Fecha'].max(), key='end_date_arribos_cargas')
+            st.write(f"Fecha Fin: {end_date_arribos_cargas.strftime('%d/%m/%Y')}")
+        with col1_3:
+            cliente_arribos_carga = st.selectbox("Cliente", options=arribos_expo_carga_historico['Cliente'].unique(), key='cliente_arribos_carga')
+            st.write(f"Cliente: {cliente_arribos_carga}")
         
         # Filter data based on selected date range
-        filtered_data_arribos = arribos_expo_carga_historico[(arribos_expo_carga_historico['Fecha'] >= pd.to_datetime(start_date_arribos)) & 
-                                                       (arribos_expo_carga_historico['Fecha'] <= pd.to_datetime(end_date_arribos))]
+        filtered_data_arribos = arribos_expo_carga_historico[(arribos_expo_carga_historico['Fecha'] >= pd.to_datetime(start_date_arribos_carga)) & 
+                                                       (arribos_expo_carga_historico['Fecha'] <= pd.to_datetime(end_date_arribos_cargas)) &
+                                                         (arribos_expo_carga_historico['Cliente'] == cliente_arribos_carga)]
         
         # Format 'Fecha' column to show only date part in Spanish format
         filtered_data_arribos['Fecha'] = filtered_data_arribos['Fecha'].dt.strftime('%d/%m/%Y')
@@ -74,14 +78,20 @@ def show_page_expo_historico():
         st.subheader("Arribos de Contenedores")
         col2_1, col2_2, col2_3 = st.columns(3)
         with col2_1:
-            start_date_arribos = st.date_input("Fecha Inicio", value=arribos_expo_ctns_historico['Fecha'].min(), key='start_date_arribos')
-            st.write(f"Fecha Inicio: {start_date_arribos.strftime('%d/%m/%Y')}")
+            start_date_arribos_ctns = st.date_input("Fecha Inicio", value=arribos_expo_ctns_historico['Fecha'].min(), key='start_date_arribos_ctns')
+            st.write(f"Fecha Inicio: {start_date_arribos_ctns.strftime('%d/%m/%Y')}")
         with col2_2:
-            end_date_arribos = st.date_input("Fecha Fin", value=arribos_expo_ctns_historico['Fecha'].max(), key='end_date_arribos')
-            st.write(f"Fecha Fin: {end_date_arribos.strftime('%d/%m/%Y')}")
+            end_date_arribos_ctns = st.date_input("Fecha Fin", value=arribos_expo_ctns_historico['Fecha'].max(), key='end_date_arribos_ctns')
+            st.write(f"Fecha Fin: {end_date_arribos_ctns.strftime('%d/%m/%Y')}")
         with col2_3:
-            cliente_arribos = st.selectbox("Cliente", options=arribos_expo_ctns_historico['Cliente'].unique(), key='cliente_arribos')
-            st.write(f"Cliente: {cliente_arribos}")
+            cliente_arribos_ctns = st.selectbox("Cliente", options=arribos_expo_ctns_historico['Cliente'].unique(), key='cliente_arribos_ctns')
+            st.write(f"Cliente: {cliente_arribos_ctns}")
+        
+        filtered_data_arribos_ctns = arribos_expo_ctns_historico.loc[(arribos_expo_ctns_historico['Fecha'] >= pd.to_datetime(start_date_arribos_ctns)) &
+                                                                    (arribos_expo_ctns_historico['Fecha'] <= pd.to_datetime(end_date_arribos_ctns)) &
+                                                                    (arribos_expo_ctns_historico['Cliente'] == cliente_arribos_ctns)]
+        filtered_data_arribos_ctns['Fecha'] = filtered_data_arribos_ctns['Fecha'].dt.strftime('%d/%m/%Y')
+        st.dataframe(filtered_data_arribos_ctns, hide_index=True, use_container_width=True)
 
         st.subheader("Otros")
         col2_4, col2_5, col2_6 = st.columns(3)
@@ -104,5 +114,4 @@ def show_page_expo_historico():
         filtered_data_otros['Dia'] = filtered_data_otros['Dia'].dt.strftime('%d/%m/%Y')
         
         st.dataframe(filtered_data_otros, hide_index=True, use_container_width=True)
-
-
+    
