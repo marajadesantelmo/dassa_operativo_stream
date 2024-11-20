bb78e973abc1005a137b14aa08fcb9e5bdedb8e9
marajadesantelmo <facundol@hotmail.com>
2024-11-20 15:04:58 -0300

    historico

--- stream_trafico_historico.py
+++ stream_trafico_historico.py
@@ -4,14 +4,14 @@ from datetime import datetime
 import time
 from utils import highlight
 
-def fetch_data_trafico():
-    trafico_entrega_vacio = pd.read_csv('data/trafico_entrega_vacio.csv')
-    trafico_carga = pd.read_csv('data/trafico_carga.csv')
+def fetch_data_trafico_historico():
+    trafico_entrega_vacio = pd.read_csv('data/trafico_entrega_vacio_historico.csv')
+    trafico_carga = pd.read_csv('data/trafico_carga_historico.csv')
     return trafico_entrega_vacio, trafico_carga
     
-def show_page_trafico():
+def show_page_trafico_historico():
     # Load data
-    trafico_entrega_vacio, trafico_carga = fetch_data_trafico()
+    trafico_entrega_vacio_historico, trafico_carga_historico = fetch_data_trafico_historico()
 
     st.markdown(
         '<p style="color: yellow; font-size: 20px;">Sección en construcción</p>',
@@ -26,15 +26,15 @@ def show_page_trafico():
         st.title(f"Orden de Tráfico")
     
     st.subheader("Contenedores")
-    st.dataframe(trafico_entrega_vacio, hide_index=True, use_container_width=True)
+    st.dataframe(trafico_entrega_vacio_historico, hide_index=True, use_container_width=True)
 
     st.subheader("Carga")
-    st.dataframe(trafico_carga, hide_index=True, use_container_width=True)
+    st.dataframe(trafico_carga_historico, hide_index=True, use_container_width=True)
     
 
 if __name__ == "__main__":
     while True:
-        show_page_trafico()
+        show_page_trafico_historico()
         time.sleep(60)  
         st.experimental_rerun() 
 
