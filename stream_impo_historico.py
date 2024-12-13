af6ee59636f4cb78423e5a2ae83fca1c6a8e8022
marajadesantelmo <facundol@hotmail.com>
2024-12-13 15:41:34 -0300

    sfs

--- stream_impo_historico.py
+++ stream_impo_historico.py
@@ -19,7 +19,7 @@ def filter_data(data, cliente, start_date, end_date, date_column):
     
     filtered_data = filtered_data[(filtered_data[date_column] >= pd.to_datetime(start_date)) & 
                                   (filtered_data[date_column] <= pd.to_datetime(end_date))]
-    filtered_data.loc[:, date_column] = filtered_data[date_column].dt.strftime('%d/%m/%Y')
+    filtered_data[date_column] = filtered_data[date_column].dt.strftime('%d/%m/%Y')
     return filtered_data
 
 def show_page_impo_historico():
