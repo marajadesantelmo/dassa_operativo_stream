ba1f419ac9044ebdd00eaea29fb0106a5e6eec34
Facundo Lastra <flastra@SYSPRO01.LOCAL>
2025-01-15 16:31:00 -0300

    Update 16:31

--- alerta_balanza.py
+++ alerta_balanza.py
@@ -1,17 +1,24 @@
 import pandas as pd
 import pyodbc
 
+clientes = pd.read_csv('contacto_clientes.csv')
+alertas_balanza = pd.read_csv('alertas_balanza.csv')
+
+today = pd.Timestamp.today().strftime('%Y-%m-%d')
+
 conn = pyodbc.connect('DRIVER={SQL Server};SERVER=101.44.8.58\\SQLEXPRESS_X86,1436;UID=dassa;PWD=Da$$a3065!')
 cursor = conn.cursor()
 
 cursor.execute(f"""
-SELECT *
+SELECT idpesada, idcliente, cl_nombre, idata, ata_nombre, entrada, salida, peso_bruto, peso_tara, peso_neto, contenedor
 FROM DEPOFIS.DASSA.BALANZA_PESADA
+WHERE fecha > '{today}'
 """)
 
 rows = cursor.fetchall()
 columns = [column[0] for column in cursor.description]
 balanza = pd.DataFrame.from_records(rows, columns=columns)
-balanza = balanza.tail(100)
+
 balanza.to_excel('ver_balanza.xlsx', index=False)
 
+balanza.columns
