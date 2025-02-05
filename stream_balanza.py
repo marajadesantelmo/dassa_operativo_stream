9b6384a5c9463db9e08bab0774905bdd7595835a
marajadesantelmo <facundol@hotmail.com>
2025-02-05 10:39:28 -0300

    xxx

--- stream_balanza.py
+++ stream_balanza.py
@@ -7,14 +7,13 @@ from utils import highlight
 @st.cache_data(ttl=60) 
 def fetch_data_balanza():
     balanza = pd.read_csv('data/balanza.csv')
-    # Convert specified columns to string
-    columns_to_convert = ['Peso Bruto', 'Peso Tara', 'Peso Neto', 'Peso Mercadería', 'Masa Verificada']
-    balanza[columns_to_convert] = balanza[columns_to_convert].astype(str)
-    return balanza
+    balanza_impo = balanza[balanza['tipo_oper'] == 'Importacion']
+    balanza_expo = balanza[balanza['tipo_oper'] == 'Exportacion']
+    return balanza_impo, balanza_expo
 
 def show_page_balanza():
     # Load data
-    balanza = fetch_data_balanza()
+    balanza_impo, balanza_expo = fetch_data_balanza()
 
     col_logo, col_title = st.columns([2, 5])
     with col_logo:
@@ -22,8 +21,10 @@ def show_page_balanza():
     with col_title:
         current_day = datetime.now().strftime("%d/%m/%Y")
         st.title(f"Operaciones en balanza del {current_day}")
-
-    st.dataframe(balanza, hide_index=True, use_container_width=True)
+    st.subheader("Importación")
+    st.dataframe(balanza_impo, hide_index=True, use_container_width=True)
+    st.subheader("Exportación")
+    st.dataframe(balanza_expo, hide_index=True, use_container_width=True)
 
 # Run the show_page function
 if __name__ == "__main__":
