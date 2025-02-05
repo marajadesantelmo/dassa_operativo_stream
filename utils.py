6fd1671f1ffb0601a55c1e984086a88fe07ee660
marajadesantelmo <facundol@hotmail.com>
2025-02-05 14:46:51 -0300

    zzzz

--- utils.py
+++ utils.py
@@ -113,8 +113,8 @@ def generar_comprobante(balanza_row):
     pdf.ln(15)
 
     pdf.set_text_color(0, 0, 0)  # Reset to black
-    pdf.set_font("Arial", size=10)
-    pdf.cell(200, 3, txt="...............                  ...............                ...............                  ..............", ln=True, align="C")
+    pdf.set_font("Arial", size=12)
+    pdf.cell(200, 3, txt="..............................                  ..............................                ..............................                  .............................", ln=True, align="C")
     pdf.cell(200, 3, txt="Administración                          Encargado                          Transporte                             Aduana", ln=True, align="C")
     pdf.ln(15)
     pdf.set_font("Arial", style='B', size=12)
