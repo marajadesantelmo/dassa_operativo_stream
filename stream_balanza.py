ddecbb2a641fb73f4ab1cc60b0bb7874ed81e138
marajadesantelmo <facundol@hotmail.com>
2025-02-05 11:59:19 -0300

    xxx

--- stream_balanza.py
+++ stream_balanza.py
@@ -31,12 +31,19 @@ def show_page_balanza():
     st.dataframe(balanza_expo, column_config={col: st.column_config.NumberColumn(col, format="%s") for col in columns_to_format}, hide_index=True, use_container_width=True)
 
     st.subheader("Generar Comprobante")
-    id_pesada = st.selectbox("Seleccione el ID de Pesada", balanza_impo['id Pesada'].tolist() + balanza_expo['id Pesada'].tolist())
+    id_pesada = st.selectbox("Seleccione el ID de Pesada", balanza['id Pesada'].tolist())
     if st.button("Generar Comprobante"):
         balanza_row = balanza[balanza['id Pesada'] == id_pesada].iloc[0] 
         pdf = generar_comprobante(balanza_row)
-        pdf_output = f"comprobante_{id_pesada}.pdf"
+        pdf_output = f"comprobante_pesada_{id_pesada}.pdf"
         pdf.output(pdf_output)
+        with open(pdf_output, "rb") as file:
+            st.download_button(
+                label="Descargar Comprobante",
+                data=file,
+                file_name=pdf_output,
+                mime="application/pdf"
+            )
         st.success(f"Comprobante generado: {pdf_output}")
 
 # Run the show_page function
