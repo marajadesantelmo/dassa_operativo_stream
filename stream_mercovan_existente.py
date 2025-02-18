2a7fd1ce7243e3b6ff886993e1a696025b67552c
marajadesantelmo <facundol@hotmail.com>
2025-02-18 14:28:22 -0300

    xxx

--- stream_mercovan_existente.py
+++ stream_mercovan_existente.py
@@ -12,11 +12,15 @@ def fetch_data_impo():
     existente_alm = existente_alm[existente_alm['Cliente'].str.contains('Mercovan')].drop(columns=['Cliente'])
     existente_plz = existente_plz.drop_duplicates()
     existente_alm = existente_alm.drop_duplicates()
-    return existente_plz, existente_alm
+    pendiente_consolidar = pd.read_csv('data/pendiente_consolidar.csv')
+    pendiente_consolidar = pendiente_consolidar[pendiente_consolidar['Cliente'].str.contains('Mercovan')].drop(columns=['Cliente'])
+    listos_para_remitir = pd.read_csv('data/listos_para_remitir.csv')
+    listos_para_remitir = listos_para_remitir[listos_para_remitir['Cliente'].str.contains('Mercovan')].drop(columns=['Cliente'])
+    return existente_plz, existente_alm, pendiente_consolidar, listos_para_remitir
 
 def show_page_existente():
     # Load data
-    existente_plz, existente_alm= fetch_data_impo()
+    existente_plz, existente_alm, pendiente_consolidar, listos_para_remitir = fetch_data_impo()
 
     col_title, col_logo, col_simpa = st.columns([5, 1, 1])
     with col_title:
@@ -36,6 +40,17 @@ def show_page_existente():
                      column_config={'e-tally': st.column_config.LinkColumn('e-tally link', 
                                                                           display_text='e-tally',)},
                      hide_index=True, use_container_width=True)
+        st.markdown("<hr>", unsafe_allow_html=True)
+
+    st.header("Estado de la carga de EXPO")
+    col4, col5 = st.columns(2)
+    with col4:
+        st.subheader("Pendiente de Consolidar")
+        st.dataframe(pendiente_consolidar, hide_index=True, use_container_width=True)
+    with col5:
+        st.subheader("Pendiente de Remitir")
+        st.dataframe(listos_para_remitir, hide_index=True, use_container_width=True)
+
 
 # Run the show_page function
 if __name__ == "__main__":
--- stream_mercovan_orden_del_dia.py
+++ stream_mercovan_orden_del_dia.py
@@ -27,15 +27,11 @@ def fetch_data_orden_del_dia():
     remisiones = remisiones[remisiones['Cliente'].str.contains('Mercovan')].drop(columns=['Cliente'])
     consolidados = pd.read_csv('data/consolidados.csv')
     consolidados = consolidados[consolidados['Cliente'].str.contains('Mercovan')].drop(columns=['Cliente'])
-    pendiente_consolidar = pd.read_csv('data/pendiente_consolidar.csv')
-    pendiente_consolidar = pendiente_consolidar[pendiente_consolidar['Cliente'].str.contains('Mercovan')].drop(columns=['Cliente'])
-    listos_para_remitir = pd.read_csv('data/listos_para_remitir.csv')
-    listos_para_remitir = listos_para_remitir[listos_para_remitir['Cliente'].str.contains('Mercovan')].drop(columns=['Cliente'])
-    return arribos, pendiente_desconsolidar, verificaciones_impo, retiros_impo, otros_impo, arribos_expo_carga, arribos_expo_ctns, verificaciones_expo, otros_expo, remisiones, consolidados, pendiente_consolidar, listos_para_remitir
+    return arribos, pendiente_desconsolidar, verificaciones_impo, retiros_impo, otros_impo, arribos_expo_carga, arribos_expo_ctns, verificaciones_expo, otros_expo, remisiones, consolidados
     
 def show_page_orden_del_dia():
     # Load data
-    arribos, pendiente_desconsolidar, verificaciones_impo, retiros_impo, otros_impo, arribos_expo_carga, arribos_expo_ctns, verificaciones_expo, otros_expo, remisiones, consolidados, pendiente_consolidar, listos_para_remitir = fetch_data_orden_del_dia()
+    arribos, pendiente_desconsolidar, verificaciones_impo, retiros_impo, otros_impo, arribos_expo_carga, arribos_expo_ctns, verificaciones_expo, otros_expo, remisiones, consolidados = fetch_data_orden_del_dia()
 
     col_title, col_logo, col_simpa = st.columns([5, 1, 1])
     with col_title:
@@ -115,16 +111,6 @@ def show_page_orden_del_dia():
         st.subheader("Otros")
         st.dataframe(otros_expo.style, hide_index=True, use_container_width=True)
 
-    st.markdown("<hr>", unsafe_allow_html=True)
-
-    st.header("Estado de la carga de EXPO")
-    col4, col5 = st.columns(2)
-    with col4:
-        st.subheader("Pendiente de Consolidar")
-        st.dataframe(pendiente_consolidar, hide_index=True, use_container_width=True)
-    with col5:
-        st.subheader("Pendiente de Remitir")
-        st.dataframe(listos_para_remitir, hide_index=True, use_container_width=True)
 
 
 if __name__ == "__main__":
