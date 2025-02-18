21d44d5f9b85c7e30a126c00a50f05cec718eae2
marajadesantelmo <facundol@hotmail.com>
2025-02-18 10:36:11 -0300

    xx

--- stream_mercovan.py
+++ stream_mercovan.py
@@ -13,27 +13,63 @@ st.set_page_config(page_title="Operativa DASSA-Mercovan",
                    page_icon="📊", 
                    layout="wide")
 
-refresh_interval_ms = 120 * 1000  # 2 minutes
-count = st_autorefresh(interval=refresh_interval_ms, limit=None, key="auto-refresh")
-
 # Estilo
 with open("styles.css") as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-page_selection = option_menu(
-        None,  # No menu title
-        ["Existente", "Orden del Día", "Histórico", "Facturación"],  
-        icons=["arrow-down-circle", "arrow-up-circle", "clock-history", "file-invoice-dollar"],
-        menu_icon="cast",  
-        default_index=0, 
-        orientation="horizontal")
-if page_selection == "Existente":
-    stream_mercovan_existente.show_page_existente()
-elif page_selection == "Orden del Día":
-    stream_mercovan_orden_del_dia.show_page_orden_del_dia()
-elif page_selection == "Histórico":
-    stream_mercovan_historico.show_page_impo_historico()
-elif page_selection == "Facturación":
-    stream_mercovan_facturacion.show_page_facturacion()
+refresh_interval_ms = 120 * 1000  # 2 minutes
+count = st_autorefresh(interval=refresh_interval_ms, limit=None, key="auto-refresh")
+
+USERNAMES = ["operativo", "administrativo"]
+PASSWORDS = ["op123", "adm123"]
+
+def login(username, password):
+    if username in USERNAMES and password in PASSWORDS:
+        return True
+    return False
+
+# Initialize cookies manager
+cookies = EncryptedCookieManager(prefix="dassa_", password="your_secret_password")
+
+if not cookies.ready():
+    st.stop()
+
+# Check if user is already logged in
+if 'logged_in' not in st.session_state:
+    st.session_state.logged_in = cookies.get("logged_in", False)
+if 'username' not in st.session_state:
+    st.session_state.username = cookies.get("username", "")
+
+if not st.session_state['logged_in']:
+    st.title("Login")
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+    if st.button("Login"):
+        if login(username, password):
+            st.session_state['logged_in'] = True
+            st.session_state.username = username
+            cookies["logged_in"] = str(True)  # Convert to string
+            cookies["username"] = username  # Username is already a string
+            cookies.save()  # Persist the changes
+            st.success("Usuario logeado")
+            st.rerun()
+        else:
+            st.error("Usuario o clave invalidos")
+else:
+    page_selection = option_menu(
+            None,  # No menu title
+            ["Existente", "Orden del Día", "Histórico", "Facturación"],  
+            icons=["arrow-down-circle", "arrow-up-circle", "clock-history", "file-invoice-dollar"],
+            menu_icon="cast",  
+            default_index=0, 
+            orientation="horizontal")
+    if page_selection == "Existente":
+        stream_mercovan_existente.show_page_existente()
+    elif page_selection == "Orden del Día":
+        stream_mercovan_orden_del_dia.show_page_orden_del_dia()
+    elif page_selection == "Histórico":
+        stream_mercovan_historico.show_page_impo_historico()
+    elif page_selection == "Facturación":
+        stream_mercovan_facturacion.show_page_facturacion()
 
 
