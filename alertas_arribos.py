d48c6e16247b9522676feafc5ea2167650185902
Facundo Lastra <flastra@SYSPRO01.LOCAL>
2025-02-21 09:51:00 -0300

    Update 09:51

--- alertas_arribos.py
+++ alertas_arribos.py
@@ -21,6 +21,33 @@ def log(mensaje):
     set_with_dataframe(worksheet_logs, df_logs)
     print("Se registró el logeo")
 
+# Function to send email
+def send_email(row, mail):
+    # Get the current time
+    current_time = datetime.now().strftime('%H:%M')
+    email_content = f"""
+    <html>
+    <body>
+        <h2>Notificación de Contenedor {row['contenedor']} Arribado</h2>
+        <p>Estimado cliente,</p>
+        <p>Le informamos que el contenedor <strong>{row['contenedor']}</strong> del cliente <strong>{row['cliente']}</strong> arribó a las instalaciones de DASSA a las <strong>{current_time}</strong>.</p>
+        <p>Saludos cordiales,</p>
+        <p><strong>Alertas automáticas - Dassa Operativo</strong></p>
+        <img src="https://dassa.com.ar/wp-content/uploads/elementor/thumbs/DASSA-LOGO-3.0-2024-PNG-TRANSPARENTE-qrm2px9hpjbdymy2y0xddhecbpvpa9htf30ikzgxds.png" alt="Dassa Logo" width="200">
+    </body>
+    </html>
+    """
+    msg = MIMEMultipart()
+    msg['Subject'] = 'Notificación de Contenedor Arribado'
+    msg['From'] = "auto@dassa.com.ar"
+    msg['To'] = mail
+    msg.attach(MIMEText(email_content, 'html'))
+    with smtplib.SMTP("smtp.gmail.com", 587) as server:
+        server.starttls()
+        server.login("auto@dassa.com.ar", "gyctvgzuwfgvmlfu")
+        server.sendmail(msg['From'], msg['To'], msg.as_string())
+
+
 arribados = pd.read_csv(path + 'alertas_arribos.csv')
 alertas = arribados[arribados['alerta_enviada'] == 0]
 arribados = arribados[arribados['alerta_enviada'] == 1]
@@ -41,71 +68,6 @@ tally_bi['idcont'] = tally_bi['idcont'].str.strip()
 
 alertas_retiros['Contenedor'] = alertas_retiros['Contenedor'].str.strip()
 
-
-# Function to send email
-def send_email(row, mail):
-    # Get the current time
-    current_time = datetime.now().strftime('%H:%M')
-    email_content = f"""
-    <html>
-    <body>
-        <h2>Notificación de Contenedor Arribado</h2>
-        <p>Estimado cliente,</p>
-        <p>Le informamos que el contenedor <strong>{row['contenedor']}</strong> del cliente <strong>{row['cliente']}</strong> arribó a las instalaciones de DASSA a las <strong>{current_time}</strong>.</p>
-        <p>Saludos cordiales,</p>
-        <p><strong>Alertas automáticas - Dassa Operativo</strong></p>
-        <img src="https://dassa.com.ar/wp-content/uploads/elementor/thumbs/DASSA-LOGO-3.0-2024-PNG-TRANSPARENTE-qrm2px9hpjbdymy2y0xddhecbpvpa9htf30ikzgxds.png" alt="Dassa Logo" width="200">
-    </body>
-    </html>
-    """
-    msg = MIMEMultipart()
-    msg['Subject'] = 'Notificación de Contenedor Arribado'
-    msg['From'] = "auto@dassa.com.ar"
-    msg['To'] = mail
-    msg.attach(MIMEText(email_content, 'html'))
-    with smtplib.SMTP("smtp.gmail.com", 587) as server:
-        server.starttls()
-        server.login("auto@dassa.com.ar", "gyctvgzuwfgvmlfu")
-        server.sendmail(msg['From'], msg['To'], msg.as_string())
-
-def send_email_retiro(row, mail, url=None):
-    current_time = datetime.now().strftime('%H:%M')
-    if url:
-        email_content = f"""
-        <html>
-        <body>
-            <h2>Notificación de Retiro de mercadería de Importación</h2>
-            <p>Estimado cliente,</p>
-            <p>Le informamos que se realizó un retiro relacionado al contenedor <strong>{row['Contenedor']}</strong> del cliente <strong>{row['Cliente']}</strong> a las <strong>{current_time}</strong>.</p>
-            <p>Para más información, por favor visite nuestro <a href="{url}">eTally</a></p>
-            <p>Saludos cordiales,</p>
-            <p><strong>Alertas automáticas - Dassa Operativo</strong></p>
-            <img src="https://dassa.com.ar/wp-content/uploads/elementor/thumbs/DASSA-LOGO-3.0-2024-PNG-TRANSPARENTE-qrm2px9hpjbdymy2y0xddhecbpvpa9htf30ikzgxds.png" alt="Dassa Logo" width="200">
-        </body>
-        </html>
-        """
-    else:
-        email_content = f"""
-        <html>
-        <body>
-            <h2>Notificación de Retiro de mercadería de Importación</h2>
-            <p>Estimado cliente,</p>
-            <p>Le informamos que se realizó un retiro relacioando al contenedor <strong>{row['Contenedor']}</strong> del cliente <strong>{row['Cliente']}</strong> a las <strong>{current_time}</strong>.</p>
-            <p>Saludos cordiales,</p>
-            <p><strong>Alertas automáticas - Dassa Operativo</strong></p>
-            <img src="https://dassa.com.ar/wp-content/uploads/elementor/thumbs/DASSA-LOGO-3.0-2024-PNG-TRANSPARENTE-qrm2px9hpjbdymy2y0xddhecbpvpa9htf30ikzgxds.png" alt="Dassa Logo" width="200">
-        </body>
-        </html>
-        """
-    msg = MIMEMultipart()
-    msg['Subject'] = 'Notificación Retiro Mercadería de Importación'
-    msg['From'] = "auto@dassa.com.ar"
-    msg['To'] = "marajadesantelmo@gmail.com"
-    msg.attach(MIMEText(email_content, 'html'))
-    with smtplib.SMTP("smtp.gmail.com", 587) as server:
-        server.starttls()
-        server.login("auto@dassa.com.ar", "gyctvgzuwfgvmlfu")
-        server.sendmail(msg['From'], msg['To'], msg.as_string())
 
 print('Enviando alertas arribos...')
 for index in range(len(alertas)):
@@ -132,42 +94,4 @@ for index in range(len(alertas)):
         arribados = pd.concat([arribados, row_df], ignore_index=True)
         arribados.to_csv(path + 'alertas_arribos.csv', index=False)
 
-print('Enviando alertas retiros...')
-
-for index in range(len(alertas_retiros)):
-    row = alertas_retiros.iloc[index]
-    client_emails = clientes[clientes['apellido'] == row['Cliente']]['email'].values
-    contenedor = row["Contenedor"]
-    idcont = tally_bi[tally_bi['cont'] == contenedor]['idcont']
-    if len(idcont) == 0:
-        log(f'No se encontró el contenedor {contenedor} en el sistema eTally')
-        print(f'No se encontró el contenedor {contenedor} en el sistema eTally')
-        url=None
-    else: 
-        url = 'http://dassa.mqbic.com/PublicTally.php?id=' + idcont
-    if len(client_emails) > 0:
-        mails = client_emails[0]
-        for mail in mails:
-            try:
-                send_email_retiro(row, mail, url)
-                log(f'Enviado a {mail} CTN {row["Contenedor"]} - Retiro IMPO')
-                print(f'Correo enviado a {mail} para el contenedor {row["Contenedor"]}')
-            except Exception as e:
-                log(f'Error al enviar correo Retiro IMPO a {mail} para el contenedor {row["Contenedor"]}: {e}')
-                print(f'Error al enviar correo Retiro IMPO a {mail} para el contenedor {row["Contenedor"]}: {e}')
-        
-        row['alerta_enviada'] = 1
-        row_df = pd.DataFrame([row])
-        retirados = pd.concat([retirados, row_df], ignore_index=True)
-        retirados.to_csv(path + 'alertas_retiros_impo.csv', index=False)
-    else:
-        log(f'No se encontraron correos electrónicos para el cliente {row["Cliente"]}')
-        row['alerta_enviada'] = 1
-        row_df = pd.DataFrame([row])
-        retirados = pd.concat([retirados, row_df], ignore_index=True)
-        retirados.to_csv(path + 'alertas_retiros_impo.csv', index=False)
-
-# Save the updated DataFrames
 arribados.to_csv(path + 'alertas_arribos.csv', index=False)
-retirados.to_csv(path + 'alertas_retiros_impo.csv', index=False)
-
