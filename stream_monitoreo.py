0bf9023a6b66f813ad83779091349dc537c4fe5e
marajadesantelmo <facundol@hotmail.com>
2025-02-26 11:03:17 -0300

    xxx

--- stream_monitoreo.py
+++ stream_monitoreo.py
@@ -67,7 +67,7 @@ col2.metric(label=existente_df.iloc[1]['Metricas'], value=existente_df.iloc[1]['
 st.header("CTNs IMPO ingresados")
 
 st.markdown("""
-    <div style="display: grid; grid-template-columns: 1fr 1fr; gap: 8px; color: darkred;">
+    <div style="display: grid; grid-template-columns: 1fr 1fr; gap: 8px;">
         <div style="display: flex; flex-direction: column; align-items: center;">
             <h5>Mes actual</h5>
             <p style="font-size: calc(1em + 1vw);">{}</p>
