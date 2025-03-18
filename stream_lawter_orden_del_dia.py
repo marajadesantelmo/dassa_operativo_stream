9842b51f3a1961082a6115e805be91e39cdd9587
marajadesantelmo <facundol@hotmail.com>
2025-03-18 12:15:56 -0300

    xxx

--- stream_lawter_orden_del_dia.py
+++ stream_lawter_orden_del_dia.py
@@ -5,79 +5,38 @@ import time
 from utils import highlight
 
 def fetch_data_orden_del_dia():
-    arribos = pd.read_csv('data/arribos.csv')
-    arribos = arribos[arribos['Cliente'].str.contains('Lawter')].drop(columns=['Cliente'])
-    pendiente_desconsolidar = pd.read_csv('data/pendiente_desconsolidar.csv')
-    pendiente_desconsolidar = pendiente_desconsolidar[pendiente_desconsolidar['Cliente'].str.contains('Lawter')].drop(columns=['Cliente'])
-    verificaciones_impo = pd.read_csv('data/verificaciones_impo.csv')
-    verificaciones_impo = verificaciones_impo[verificaciones_impo['Cliente'].str.contains('Lawter')].drop(columns=['Cliente'])
-    retiros_impo = pd.read_csv('data/retiros_impo.csv')
-    retiros_impo = retiros_impo[retiros_impo['Cliente'].str.contains('Lawter')].drop(columns=['Cliente'])
-    otros_impo = pd.read_csv('data/otros_impo.csv')
-    otros_impo = otros_impo[otros_impo['Cliente'].str.contains('Lawter')].drop(columns=['Cliente'])
     arribos_expo_carga = pd.read_csv('data/arribos_expo_carga.csv')
-    arribos_expo_carga = arribos_expo_carga[arribos_expo_carga['Cliente'].str.contains('Lawter')].drop(columns=['Cliente'])
+    arribos_expo_carga = arribos_expo_carga[arribos_expo_carga['Cliente']== 'Lawter Argentina S.A.'].drop(columns=['Cliente'])
     arribos_expo_ctns = pd.read_csv('data/arribos_expo_ctns.csv')
-    arribos_expo_ctns = arribos_expo_ctns[arribos_expo_ctns['Cliente'].str.contains('Lawter')].drop(columns=['Cliente'])
+    arribos_expo_ctns = arribos_expo_ctns[arribos_expo_ctns['Cliente']== 'Lawter Argentina S.A.'].drop(columns=['Cliente'])
     verificaciones_expo = pd.read_csv('data/verificaciones_expo.csv')
-    verificaciones_expo = verificaciones_expo[verificaciones_expo['Cliente'].str.contains('Lawter')].drop(columns=['Cliente'])
+    verificaciones_expo = verificaciones_expo[verificaciones_expo['Cliente']== 'Lawter Argentina S.A.'].drop(columns=['Cliente'])
     otros_expo = pd.read_csv('data/otros_expo.csv')
-    otros_expo = otros_expo[otros_expo['Cliente'].str.contains('Lawter')].drop(columns=['Cliente'])
+    otros_expo = otros_expo[otros_expo['Cliente']== 'Lawter Argentina S.A.'].drop(columns=['Cliente'])
     remisiones = pd.read_csv('data/remisiones.csv')
-    remisiones = remisiones[remisiones['Cliente'].str.contains('Lawter')].drop(columns=['Cliente'])
+    remisiones = remisiones[remisiones['Cliente']== 'Lawter Argentina S.A.'].drop(columns=['Cliente'])
     consolidados = pd.read_csv('data/consolidados.csv')
-    consolidados = consolidados[consolidados['Cliente'].str.contains('Lawter')].drop(columns=['Cliente'])
-    return arribos, pendiente_desconsolidar, verificaciones_impo, retiros_impo, otros_impo, arribos_expo_carga, arribos_expo_ctns, verificaciones_expo, otros_expo, remisiones, consolidados
+    consolidados = consolidados[consolidados['Cliente']== 'Lawter Argentina S.A.'].drop(columns=['Cliente'])
+    pendiente_consolidar = pd.read_csv('data/pendiente_consolidar.csv')
+    pendiente_consolidar = pendiente_consolidar[pendiente_consolidar['Cliente']== 'Lawter Argentina S.A.'].drop(columns=['Cliente'])
+    listos_para_remitir = pd.read_csv('data/listos_para_remitir.csv')
+    listos_para_remitir = listos_para_remitir[listos_para_remitir['Cliente']== 'Lawter Argentina S.A.'].drop(columns=['Cliente'])
+    return  arribos_expo_carga, arribos_expo_ctns, verificaciones_expo, otros_expo, remisiones, consolidados, pendiente_consolidar, listos_para_remitir
     
 def show_page_orden_del_dia():
     # Load data
-    arribos, pendiente_desconsolidar, verificaciones_impo, retiros_impo, otros_impo, arribos_expo_carga, arribos_expo_ctns, verificaciones_expo, otros_expo, remisiones, consolidados = fetch_data_orden_del_dia()
+    arribos_expo_carga, arribos_expo_ctns, verificaciones_expo, otros_expo, remisiones, consolidados, pendiente_consolidar, listos_para_remitir = fetch_data_orden_del_dia()
 
     col_title, col_logo, col_simpa = st.columns([5, 1, 1])
     with col_title:
         current_day = datetime.now().strftime("%d/%m/%Y")
-        st.header(f"Operaciones de IMPO a partir del {current_day}")
+        st.header(f"Operaciones de EXPO a partir del {current_day}")
     with col_logo:
         st.image('logo.png')
     with col_simpa:
         st.image('logo_lawter.png')
     col1, col2 = st.columns(2)
 
-    col1, col2 = st.columns(2)
-
-    with col1:
-        col1_sub, col1_metric = st.columns([7, 1])
-        with col1_sub:
-            st.subheader("Arribos Contenedores día de hoy")
-        with col1_metric:
-            ctns_pendientes = arribos[(arribos['Estado'] != '-') & (~arribos['Estado'].str.contains('Arribado'))].shape[0]
-            st.metric(label="CTNs pendientes", value=ctns_pendientes)
-        st.dataframe(arribos.style.apply(highlight, axis=1), hide_index=True, use_container_width=True)
-
-    with col2:
-        col2_sub, col2_metric1, col2_mentric2 = st.columns([6, 1, 1])
-        with col2_sub:
-            st.subheader("Pendiente Desconsolidar y Vacios")
-        with col2_metric1:
-            st.metric(label="Ptes. Desco.", value=pendiente_desconsolidar[pendiente_desconsolidar['Estado'] == 'Pte. Desc.'].shape[0])
-        with col2_mentric2:
-            st.metric(label="Vacios", value=pendiente_desconsolidar[pendiente_desconsolidar['Estado'] == 'Vacio'].shape[0])
-        st.dataframe(pendiente_desconsolidar.style.apply(highlight, axis=1).format(precision=0), hide_index=True, use_container_width=True)
-
-    col3, col4 = st.columns(2)
-    with col3:
-        st.subheader("Verificaciones")
-        st.dataframe(verificaciones_impo.style.apply(highlight, axis=1), hide_index=True, use_container_width=True)
-        st.subheader("Otros")
-        st.dataframe(otros_impo.style.apply(highlight, axis=1), hide_index=True, use_container_width=True)
-
-    with col4:
-        st.subheader("Retiros")
-        st.dataframe(retiros_impo.style.apply(highlight, axis=1), hide_index=True, use_container_width=True)
-
-    st.markdown("<hr>", unsafe_allow_html=True)
-
-    st.header(f"Operaciones de EXPO")
 
     # Create two columns
     col1, col2 = st.columns(2)
@@ -106,7 +65,15 @@ def show_page_orden_del_dia():
         st.subheader("Otros")
         st.dataframe(otros_expo.style, hide_index=True, use_container_width=True)
 
-
+    
+    st.header("Estado de la carga de EXPO")
+    col4, col5 = st.columns(2)
+    with col4:
+        st.subheader("Pendiente de Consolidar")
+        st.dataframe(pendiente_consolidar, hide_index=True, use_container_width=True)
+    with col5:
+        st.subheader("Pendiente de Remitir")
+        st.dataframe(listos_para_remitir, hide_index=True, use_container_width=True)
 
 if __name__ == "__main__":
     while True:
