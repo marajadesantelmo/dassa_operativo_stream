ac43defc90ba3d256c3b3efe8f327d9fdc647048
Facundo Lastra <flastra@SYSPRO01.LOCAL>
2025-05-15 19:11:00 -0300

    Update 19:11

--- get_tallyBI.py
+++ get_tallyBI.py
@@ -1,3 +1,7 @@
+"""
+Descarga TallyBI.csv y TallyBIEXPO.csv desde servidor remoto de MCUBIC
+y procesa los datos para su posterior uso en Streamlit."""
+
 import os
 import paramiko
 import scp
@@ -19,43 +23,34 @@ else:
 ssh = paramiko.SSHClient()
 ssh.load_system_host_keys()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
-# Connect to the remote server
 ssh.connect(hostname='155.138.143.232', username=username_scp, password=password_scp)
-
-# Use SCP to get a file from the remote server
 with SCPClient(ssh.get_transport()) as scp:
     scp.get('TallyBI.csv')
+    scp.get('TallyBIEXPO.csv')
 
-# Open TallyBi.csv
+# Procesamiento datos de IMPO
 datos_tally = pd.read_csv('TallyBI.csv', sep='|', encoding='latin1')
 datos_tally = datos_tally.drop(index=0)
 datos_tally.columns = datos_tally.columns.str.strip()
-
 datos_tally['descripcion'] = datos_tally['descripcion'].str.strip()
 datos_tally['inicio'] = datos_tally['inicio'].str.strip()
-
-# Extract date from 'inicio' and create a new column 'inicio_date'
 datos_tally['fecha'] = pd.to_datetime(datos_tally['inicio'], format='%d-%m-%y %H:%M', errors='coerce').dt.date
-
 datos_tally['inicio'] = pd.to_datetime(datos_tally['inicio'], format='%d-%m-%y %H:%M', errors='coerce')
-
 datos_tally['cierre'] = datos_tally['cierre'].str.strip()
 datos_tally['cierre'] = pd.to_datetime(datos_tally['cierre'], format='%d-%m-%y %H:%M', errors='coerce')
-
 datos_tally['duracion'] = datos_tally['cierre'] - datos_tally['inicio'] 
-
-# Assuming 'duracion' is a timedelta object, first convert it to string format
 datos_tally['duracion_hhmmss'] = datos_tally['duracion'].astype(str).str[7:9] + ':' + \
                                  datos_tally['duracion'].astype(str).str[10:12] + ':' + \
                                  datos_tally['duracion'].astype(str).str[13:15]
-
 datos_tally = datos_tally.dropna(subset=['inicio'])
 datos_tally = datos_tally.dropna(subset=['cierre'])
-# Delete the first row
-
 datos_tally.to_csv('data/tally.csv', sep=',', encoding='latin1', index=False)
 
+# Procesamiento datos de EXPO
+datos_tally_expo = pd.read_csv('TallyBIEXPO.csv', sep='|', encoding='latin1')
+
+
+# Logeo
 gc = gspread.service_account(filename='credenciales_gsheets.json')
 sheet_logs =  gc.open_by_url('https://docs.google.com/spreadsheets/d/1aPUkhige3tq7_HuJezTYA1Ko7BWZ4D4W0sZJtsTyq3A')                                           
 worksheet_logs = sheet_logs.worksheet('Logeos')
