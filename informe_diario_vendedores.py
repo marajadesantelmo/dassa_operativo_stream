6635b7aa5fc39a039159098865445ed97c05a1ac
Facundo Lastra <flastra@SYSPRO01.LOCAL>
2025-05-21 09:21:00 -0300

    Update 09:21

--- informe_diario_vendedores.py
+++ informe_diario_vendedores.py
@@ -3,6 +3,38 @@ import pyodbc
 from datetime import datetime
 from tokens import username, password
 import os
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+def send_email_vendedor(row, mail, url=None):
+    current_time = datetime.now().strftime('%H:%M')
+    if url is not None:
+        email_content = f"""
+        <html>
+        <body>
+            <h2>Notificación de Remisión de Contenedor de Exportación</h2>
+            <p>Estimado cliente,</p>
+            <p>Le informamos que el contenedor <strong>{row['Contenedor']}</strong> con Booking <strong>{row['Booking']}</strong> del cliente <strong>{row['Cliente']}</strong> egresó de las instalaciones de DASSA.</p>
+            <p>Para más información, por favor visite nuestro <a href="{url.iloc[0]}">eTally</a></p>
+            <p>Saludos cordiales,</p>
+            <p><strong>Alertas automáticas - Dassa Operativo</strong></p>
+            <img src="https://dassa.com.ar/wp-content/uploads/elementor/thumbs/DASSA-LOGO-3.0-2024-PNG-TRANSPARENTE-qrm2px9hpjbdymy2y0xddhecbpvpa9htf30ikzgxds.png" alt="Dassa Logo" width="200">
+        </body>
+        </html>
+        """
+    msg = MIMEMultipart()
+    msg['Subject'] = f'Operaciones de tus clientes para el día de mañana'
+    msg['From'] = "auto@dassa.com.ar"
+    msg['To'] = mail
+    #msg['To'] = 'marajadesantelmo@gmail.com'
+    msg.attach(MIMEText(email_content, 'html'))
+    with smtplib.SMTP("smtp.gmail.com", 587) as server:
+        server.starttls()
+        server.login("auto@dassa.com.ar", "gyctvgzuwfgvmlfu")
+        server.sendmail(msg['From'], msg['To'], msg.as_string())
+
+
 
 server = '101.44.8.58\\SQLEXPRESS_X86,1436'
 conn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';UID='+username+';PWD='+ password)
@@ -10,15 +42,15 @@ cursor = conn.cursor()
 
 print('Clientes Global comex')
 cursor.execute(f"""
-SELECT  apellido, clie_nro, vendedor
+SELECT  apellido AS Cliente, clie_nro, vendedor
 FROM DEPOFIS.DASSA.[Clientes]
 """) 
 rows = cursor.fetchall()
 columns = [column[0] for column in cursor.description]
 clientes= pd.DataFrame.from_records(rows, columns=columns)
 
-clientes['apellido'] = clientes['apellido'].str.strip().str.title()
-clientes['apellido'] = clientes['apellido'].apply(lambda x: x[:20] + "..." if len(x) > 20 else x)
+clientes['Cliente'] = clientes['Cliente'].str.strip().str.title()
+clientes['Cliente'] = clientes['Cliente'].apply(lambda x: x[:20] + "..." if len(x) > 20 else x)
 
 dic_vendedores = pd.read_excel('diccionario_vendedores_puros_dassa.xlsx')
 
@@ -30,4 +62,19 @@ verificaciones_expo = pd.read_csv('data/verificaciones_expo.csv')
 remisiones_expo = pd.read_csv('data/remisiones.csv')
 consolidados_expo = pd.read_csv('data/consolidados.csv')
 otros_expo = pd.read_csv('data/otros_expo.csv')
+
+vendedores = dic_vendedores['nombre_vendedor'].unique()
+
+for vendedor in vendedores:
+    tabla_vendedor = dic_vendedores[dic_vendedores['nombre_vendedor'] == vendedor]
+    vendedor_ids = tabla_vendedor['cod_vendedor'].unique()
+    clientes_vendedor = clientes[clientes['vendedor'].isin(vendedor_ids)]['Cliente'].unique()
+    verificaciones_impo_vendedor = verificaciones_impo[verificaciones_impo['Cliente'].isin(clientes_vendedor)]
+    retiros_impo_vendedor = retiros_impo[retiros_impo['Cliente'].isin(clientes_vendedor)]
+    otros_impo_vendedor = otros_impo[otros_impo['Cliente'].isin(clientes_vendedor)]
+    verificaciones_expo_vendedor = verificaciones_expo[verificaciones_expo['Cliente'].isin(clientes_vendedor)]
+    remisiones_expo_vendedor = remisiones_expo[remisiones_expo['Cliente'].isin(clientes_vendedor)]
+    consolidados_expo_vendedor = consolidados_expo[consolidados_expo['Cliente'].isin(clientes_vendedor)]
+    otros_expo_vendedor = otros_expo[otros_expo['Cliente'].isin(clientes_vendedor)]
+
 
