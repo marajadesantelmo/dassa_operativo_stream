9c23396c3de10050b8ca610fe312e6296ff406e6
marajadesantelmo <facundol@hotmail.com>
2025-06-09 08:10:52 -0300

    xxx

--- supabase_connection.py
+++ supabase_connection.py
@@ -3,7 +3,6 @@ import pandas as pd
 import os
 from datetime import datetime
 from supabase import create_client, Client
-from dotenv import load_dotenv
 
 url_supabase = os.getenv("url_supabase")
 key_supabase= os.getenv("key_supabase")
