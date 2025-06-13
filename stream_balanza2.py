56385f3baf9a8a486020233c9cdd03ea6145b0a3
marajadesantelmo <facundol@hotmail.com>
2025-06-13 18:01:39 -0300

    balanza2

--- stream_balanza2.py
+++ stream_balanza2.py
@@ -43,18 +43,6 @@ def fetch_last_update():
         last_update = f.read().strip()
     return last_update
 
-@st.cache_data(ttl=60)
-def fetch_last_update():
-    update_log = fetch_table_data("update_log")
-    if not update_log.empty:
-        try:
-            last_update = update_log[update_log['table_name'] == 'Balanza']['last_update'].max()
-            return pd.to_datetime(last_update).strftime("%d/%m/%Y %H:%M")
-        except Exception as e:
-            st.error(f"Error al obtener la última actualización: {e}")
-            return "No disponible"
-    return "No disponible"
-
 def show_page_balanza():
     # Load data
     balanza, balanza_impo, balanza_expo, balanza_historico_impo, balanza_historico_expo, balanza_historico = fetch_data_balanza()
