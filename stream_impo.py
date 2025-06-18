eaaba4062d0bff04fd0a6405aad8d076b7a2c74d
marajadesantelmo <facundol@hotmail.com>
2025-06-18 16:17:17 -0300

    xxx

--- stream_impo.py
+++ stream_impo.py
@@ -7,7 +7,10 @@ from supabase_connection import fetch_table_data
 @st.cache_data(ttl=60) 
 def fetch_data_impo():
     arribos = fetch_table_data("arribos")
-    #arribos = arribos.sort_values(by="Turno")  # Sort by Turno
+    arribos = arribos.sort_values(by="Turno") 
+    arribos = pd.concat([
+    arribos[arribos['Estado'].str.contains('anterior', na=False)],
+    arribos[~arribos['Estado'].str.contains('anterior', na=False)]], ignore_index=True)
     pendiente_desconsolidar = fetch_table_data("pendiente_desconsolidar")
     verificaciones_impo = fetch_table_data("verificaciones_impo")
     retiros_impo = fetch_table_data("retiros_impo")
