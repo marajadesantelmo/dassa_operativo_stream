c52cf4d8958bcdb8653f6cb0dbba45fa1cf57f30
marajadesantelmo <facundol@hotmail.com>
2025-07-21 13:43:23 -0300

    xxx

--- henn/stream_henn_historico.py
+++ henn/stream_henn_historico.py
@@ -61,18 +61,6 @@ def show_page_historico():
         
         st.dataframe(filtered_data_arribos, hide_index=True, use_container_width=True)
 
-        st.subheader("Verificaciones")
-        col1_4, col1_5, col1_6 = st.columns(3)
-        with col1_4:
-            start_date_verificaciones = st.date_input("Fecha Inicio", value='2024-10-23', key='start_date_verificaciones')
-            st.write(f"Fecha Inicio: {start_date_verificaciones.strftime('%d/%m/%Y')}")
-        with col1_5:
-            end_date_verificaciones = st.date_input("Fecha Fin", value='2025-12-31', key='end_date_verificaciones')
-            st.write(f"Fecha Fin: {end_date_verificaciones.strftime('%d/%m/%Y')}")
-        with col1_6:
-            filtered_data_verificaciones = filter_data(historico_verificaciones_expo, start_date_verificaciones, end_date_verificaciones, "Dia")
-        st.dataframe(filtered_data_verificaciones, hide_index=True, use_container_width=True)
-
     with col2:
         st.subheader("Arribos de Contenedores")
         col2_1, col2_2, col2_3 = st.columns(3)
