8ccfe5bca39519362025a5351997908d88e3d238
marajadesantelmo <facundol@hotmail.com>
2025-07-28 15:54:43 -0300

    xxx

--- stream_trafico.py
+++ stream_trafico.py
@@ -14,6 +14,7 @@ def fetch_data_trafico():
     arribos_expo_ctns['Fecha'] = pd.to_datetime(arribos_expo_ctns['Fecha'], format='%d/%m')
     arribos_expo_ctns = arribos_expo_ctns.sort_values(by="Fecha")
     arribos_expo_ctns['Fecha'] = arribos_expo_ctns['Fecha'].dt.strftime('%d/%m')
+    remisiones = fetch_table_data("remisiones")
     remisiones = remisiones[remisiones['Dia'] != '-']
     if not remisiones.empty:
         remisiones['Dia'] = pd.to_datetime(remisiones['Dia'], format='%d/%m', errors='coerce')
