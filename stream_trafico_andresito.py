56ead1d1a1eab9b9e3dd03ebe8039bfac8380dd2
Facundo Lastra <flastra@SYSPRO01.LOCAL>
2025-08-06 14:41:01 -0300

    Update 14:41

--- stream_trafico_andresito.py
+++ stream_trafico_andresito.py
@@ -10,7 +10,7 @@ def fetch_data_trafico_andresito():
     arribos = fetch_table_data("trafico_arribos")
     arribos['Registro'] = pd.to_datetime(arribos['fecha_registro']) - pd.Timedelta(hours=3)
     arribos['Registro'] = arribos['Registro'].dt.strftime('%d/%m/%Y %H:%M')
-    arribos = arribos.drop(columns=['fecha_registro','key'], errors='ignore')
+    arribos = arribos.drop(columns=['fecha_registro','key', 'Tiempo'], errors='ignore')
     pendiente_desconsolidar = fetch_table_data("trafico_pendiente_desconsolidar")
     pendiente_desconsolidar['Registro'] = pd.to_datetime(pendiente_desconsolidar['fecha_registro']) - pd.Timedelta(hours=3)
     pendiente_desconsolidar['Registro'] = pendiente_desconsolidar['Registro'].dt.strftime('%d/%m/%Y %H:%M')
