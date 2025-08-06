e494f3f311596a03fa740243d3f590144e6ac0ce
marajadesantelmo <facundol@hotmail.com>
2025-08-06 14:28:27 -0300

    xxx

--- stream_trafico_andresito.py
+++ stream_trafico_andresito.py
@@ -105,7 +105,7 @@ def show_page_trafico_andresito():
                         else:
                             st.warning("Por favor ingrese el nombre del chofer")
         pendiente_desconsolidar_display = pendiente_desconsolidar.copy()
-        pendiente_desconsolidar_display['ID'] = pendiente_desconsolidar_display['id'].apply(lambda x: f"I{x:03d}")
+        pendiente_desconsolidar_display['ID'] = pendiente_desconsolidar_display['id'].apply(lambda x: f"V{x:03d}")
         cols = ['ID'] + [col for col in pendiente_desconsolidar_display.columns if col != 'ID']
         pendiente_desconsolidar_display = pendiente_desconsolidar_display[cols]
         pendiente_desconsolidar_display = pendiente_desconsolidar_display.drop(columns=['id'], errors='ignore')
@@ -149,7 +149,7 @@ def show_page_trafico_andresito():
                         else:
                             st.warning("Por favor ingrese el nombre del chofer")
         arribos_expo_ctns_display = arribos_expo_ctns.copy()
-        arribos_expo_ctns_display['ID'] = arribos_expo_ctns_display['id'].apply(lambda x: f"I{x:03d}")
+        arribos_expo_ctns_display['ID'] = arribos_expo_ctns_display['id'].apply(lambda x: f"E{x:03d}")
         cols = ['ID'] + [col for col in arribos_expo_ctns_display.columns if col != 'ID']
         arribos_expo_ctns_display = arribos_expo_ctns_display[cols]
         arribos_expo_ctns_display = arribos_expo_ctns_display.drop(columns=['id'], errors='ignore')
@@ -189,7 +189,7 @@ def show_page_trafico_andresito():
                         else:
                             st.warning("Por favor ingrese el nombre del chofer")
         remisiones_display = remisiones.copy()
-        remisiones_display['ID'] = remisiones_display['id'].apply(lambda x: f"I{x:03d}")
+        remisiones_display['ID'] = remisiones_display['id'].apply(lambda x: f"B{x:03d}")
         cols = ['ID'] + [col for col in remisiones_display.columns if col != 'ID']
         remisiones_display = remisiones_display[cols]
         remisiones_display = remisiones_display.drop(columns=['id'], errors='ignore')
