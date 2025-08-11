fbf4cafdf2215a6608e66d1ca8fa4c8d034d138d
marajadesantelmo <facundol@hotmail.com>
2025-08-11 10:17:16 -0300

    xxx

--- stream_balanza.py
+++ stream_balanza.py
@@ -3,7 +3,7 @@ import pandas as pd
 from datetime import datetime
 from supabase_connection import fetch_table_data
 from utils import highlight, generar_comprobante
-import time
+
 
 
 @st.cache_data(ttl=60)
@@ -22,26 +22,40 @@ def fetch_data_balanza():
     balanza_impo = balanza_impo.drop(columns=['tipo_oper'], errors='ignore')
     balanza_expo = balanza[balanza['tipo_oper'] == 'Exportacion']
     balanza_expo = balanza_expo.drop(columns=['tipo_oper'], errors='ignore')
-
-    return balanza, balanza_impo, balanza_expo
+    balanza_historico = pd.read_csv('data/historico_balanza.csv')
+    balanza_historico['DNI'] = balanza_historico['DNI'].fillna('-').astype(str).str.replace('.0', '', regex=False)
+    balanza_historico = balanza_historico.fillna("-")
+    balanza_historico_impo = balanza_historico[balanza_historico['tipo_oper'] == 'Importacion']
+    balanza_historico_impo = balanza_historico_impo.drop(columns=['tipo_oper'], errors='ignore')
+    balanza_historico_expo = balanza_historico[balanza_historico['tipo_oper'] == 'Exportacion']
+    balanza_historico_expo = balanza_historico_expo.drop(columns=['tipo_oper'], errors='ignore')
+    columns_impo_historico = ['ID Pesada', 'Fecha', 'Cliente', 'ATA', 'Contenedor', 'Entrada', 'Salida', 'Peso Bruto', 'Peso Tara',
+       'Peso Neto', 'Tara CNT', 'Peso Mercadería', 'Descripción', 'Patente Chasis', 'Patente Semi', 'Chofer', 'DNI',
+       'Booking', 'Precinto', 'Tipo Doc', 'Estado']
+    columns_expo_historico = ['ID Pesada', 'Fecha', 'Cliente', 'ATA',  'Entrada', 'Salida', 'Peso Bruto', 'Peso Tara',
+       'Peso Neto', 'Peso Mercadería', 'Descripción', 'Patente Chasis', 'Patente Semi', 'Chofer', 'DNI', 'Observaciones',
+       'Booking', 'Permiso Emb.', 'Tipo Doc', 'Estado']
+    balanza_historico_impo = balanza_historico_impo[columns_impo_historico]
+    balanza_historico_expo = balanza_historico_expo[columns_expo_historico]
+    balanza_historico_impo = balanza_historico_impo.sort_values(by='Estado', ascending=True)
+    balanza_historico_expo = balanza_historico_expo.sort_values(by='Estado', ascending=True)
+    return balanza, balanza_impo, balanza_expo, balanza_historico_impo, balanza_historico_expo, balanza_historico
 
 @st.cache_data(ttl=60)
 def fetch_last_update():
     update_log = fetch_table_data("update_log")
     if not update_log.empty:
-        last_update = update_log[update_log['table_name'] == 'Arribos y existente']['last_update'].max()
         try:
-            datetime_obj = pd.to_datetime(last_update)
-            if pd.isna(datetime_obj):
-                return "No disponible"
-            return datetime_obj.strftime("%d/%m/%Y %H:%M")
-        except (ValueError, TypeError):
+            last_update = update_log[update_log['table_name'] == 'Balanza']['last_update'].max()
+            return pd.to_datetime(last_update).strftime("%d/%m/%Y %H:%M")
+        except Exception as e:
+            st.error(f"Error al obtener la última actualización: {e}")
             return "No disponible"
     return "No disponible"
 
 def show_page_balanza():
     # Load data
-    balanza, balanza_impo, balanza_expo = fetch_data_balanza()
+    balanza, balanza_impo, balanza_expo, balanza_historico_impo, balanza_historico_expo, balanza_historico = fetch_data_balanza()
     last_update = fetch_last_update()
 
     col_logo, col_title = st.columns([2, 5])
@@ -76,6 +90,62 @@ def show_page_balanza():
     # Load data
 
     
+    st.title("Histórico de Pesadas")
+    
+    col1, col2, col3, col4 = st.columns(4)
+    with col1:
+        start_date_historico = pd.to_datetime(st.date_input("Fecha Inicio", value=pd.to_datetime(balanza_historico_impo['Fecha'], format='%d/%m/%Y').min(), key='start_date_historico'))
+    with col2:
+        end_date_historico = pd.to_datetime(st.date_input("Fecha Fin", value=pd.to_datetime(balanza_historico_impo['Fecha'], format='%d/%m/%Y').max(), key='end_date_historico'))
+    with col3:
+        id_pesada_filter = st.selectbox("ID Pesada", options=["Todos"] + sorted(balanza_historico_impo['ID Pesada'].unique().tolist()), key='id_pesada_filter')
+    with col4:
+        # Ensure Cliente column is treated as strings
+        cliente_options = ["Todos"] + sorted(balanza_historico_impo['Cliente'].astype(str).unique().tolist())
+        cliente_filter = st.selectbox("Cliente", options=cliente_options, key='cliente_filter')
+
+    st.write("Importación")
+    # Filter data based on the selected criteria
+    filtered_historico_impo = balanza_historico_impo[
+        (pd.to_datetime(balanza_historico_impo['Fecha'], format='%d/%m/%Y') >= start_date_historico) &
+        (pd.to_datetime(balanza_historico_impo['Fecha'], format='%d/%m/%Y') <= end_date_historico)
+    ]
+    if id_pesada_filter != "Todos":
+        filtered_historico_impo = filtered_historico_impo[filtered_historico_impo['ID Pesada'] == id_pesada_filter]
+    if cliente_filter != "Todos":
+        filtered_historico_impo = filtered_historico_impo[filtered_historico_impo['Cliente'].astype(str) == cliente_filter]
+
+    st.dataframe(filtered_historico_impo, column_config={col: st.column_config.NumberColumn(col, format="%s") for col in columns_to_format}, hide_index=True, use_container_width=True)
+
+    st.write("Exportación")
+    
+    # Repeat the same filtering logic for export data
+    filtered_historico_expo = balanza_historico_expo[
+        (pd.to_datetime(balanza_historico_expo['Fecha'], format='%d/%m/%Y') >= start_date_historico) &
+        (pd.to_datetime(balanza_historico_expo['Fecha'], format='%d/%m/%Y') <= end_date_historico)
+    ]
+    if id_pesada_filter != "Todos":
+        filtered_historico_expo = filtered_historico_expo[filtered_historico_expo['ID Pesada'] == id_pesada_filter]
+    if cliente_filter != "Todos":
+        filtered_historico_expo = filtered_historico_expo[filtered_historico_expo['Cliente'].astype(str) == cliente_filter]
+
+    st.dataframe(filtered_historico_expo, column_config={col: st.column_config.NumberColumn(col, format="%s") for col in columns_to_format}, hide_index=True, use_container_width=True)
+
+    st.subheader("Generar Comprobante")
+    id_pesada_historico = st.selectbox("Seleccione el ID de Pesada Histórico", balanza_historico['ID Pesada'].tolist())
+    if st.button("Generar Comprobante Histórico"):
+        balanza_row_historico = balanza_historico[balanza_historico['ID Pesada'] == id_pesada_historico].iloc[0] 
+        pdf_historico = generar_comprobante(balanza_row_historico)
+        pdf_output_historico = f"comprobante_pesada_historico_{id_pesada_historico}.pdf"
+        pdf_historico.output(pdf_output_historico)
+        with open(pdf_output_historico, "rb") as file:
+            st.download_button(
+                label="Descargar Comprobante Histórico",
+                data=file,
+                file_name=pdf_output_historico,
+                mime="application/pdf"
+            )
+        st.success(f"Comprobante histórico generado: {pdf_output_historico}")
 
 # Run the show_page function
 if __name__ == "__main__":
