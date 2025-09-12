b55f56934b4e72b58fc6efffe045ab9fe5a13f9a
marajadesantelmo <50368116+marajadesantelmo@users.noreply.github.com>
2025-09-12 16:20:25 -0300

    xxx

--- stream_buques.py
+++ stream_buques.py
@@ -7,8 +7,14 @@ from utils import highlight, generar_comprobante
 @st.cache_data(ttl=60)
 def fetch_data_balanza():
     apm_terminals = fetch_table_data("apm_terminals")
+    apm_terminals.rename(columns={"dias_para_arribo": "Dias para arribo"}, inplace=True)
+    apm_terminals.sort_values(by="Dias para arribo", inplace=True)
     itl_exolgan = fetch_table_data("itl_exolgan")
+    itl_exolgan.rename(columns={"dias_para_arribo": "Dias para arribo"}, inplace=True)
+    itl_exolgan.sort_values(by="Dias para arribo", inplace=True)
     dp_world = fetch_table_data("dp_world")
+    dp_world.rename(columns={"dias_para_arribo": "Dias para arribo"}, inplace=True)
+    dp_world.sort_values(by="Dias para arribo", inplace=True)
     return apm_terminals, itl_exolgan, dp_world
 
 
