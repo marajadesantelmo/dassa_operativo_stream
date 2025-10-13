bbcaf25546d169dfadd7c9f2ecd7f514bae819bb
marajadesantelmo <50368116+marajadesantelmo@users.noreply.github.com>
2025-10-13 16:58:00 -0300

    viendo impo

--- stream_impo.py
+++ stream_impo.py
@@ -165,7 +165,7 @@ def show_page_impo(allowed_clients=None, apply_mudanceras_filter=False):
         if st.session_state['username'] != "plazoleta":
             st.write("Carga suelta")
             st.dataframe(retiros_impo_carga.style.apply(highlight, axis=1), 
-                        column_config={'e-tally': st.column_config.LinkColumn('e-tally', display_text="\U0001F517",), 
+                        column_config={
                                         'Salida': st.column_config.LinkColumn('Salida', display_text="\U0001F517",)},
                         hide_index=True, use_container_width=True)
     
