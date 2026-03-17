811dbf97a03fdaf7b53d9c70f2791d9afcde0dfc
Facundo Lastra <flastra@SYSPRO01.LOCAL>
2026-03-17 09:26:20 -0300

    Update 09:26

--- stream_expo.py
+++ stream_expo.py
@@ -86,7 +86,7 @@ def show_page_expo(allowed_clients=None, apply_mudanceras_filter=False):
     
     # Apply mudanceras filter if needed
     if apply_mudanceras_filter:
-        mudanceras_filter =  ['Mercovan', 'Lift Van', 'Edelweiss',  'Rsm', 'Fenisan', 'Moniport', 'Bymar', 'Noah']
+        mudanceras_filter =  ['Mercovan', 'Lift Van', 'Edelweiss',  'Rsm', 'Fenisan', 'Moniport', 'Bymar', 'Noah', 'Baires International']
         arribos_expo_carga = arribos_expo_carga[arribos_expo_carga['Cliente'].str.contains('|'.join(mudanceras_filter), case=False, na=False)]
         arribos_expo_ctns = arribos_expo_ctns[arribos_expo_ctns['Cliente'].str.contains('|'.join(mudanceras_filter), case=False, na=False)]
         verificaciones_expo = verificaciones_expo[verificaciones_expo['Cliente'].str.contains('|'.join(mudanceras_filter), case=False, na=False)]
